58db9e185463efc1be87fd02d3a36dde17c7f190
AldairAcero <zaracero68@gmail.com>
2023-10-13 19:21:55 -0600

    socket removed

--- index.js
+++ index.js
@@ -14,32 +14,16 @@ const orderController = require("./controller/orderController");
 const port = 3001;
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
 app.use(cors());
 app.use(express.json());
 
 const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
 
 /*CONTROLLERS*/
 app.use("/product", productsController);
 app.use("/user", userController);
 app.use("/login", loginController);
 app.use("/order", orderController);
-
-io.on("connection", (socket) => {
-  console.log("connected to ws:" + socket.id);
-
-  socket.on("send_order", (data) => {
-    //console.log(data);
-    socket.broadcast.emit("send_order", (data));
-  });
-});
 
 mongoose
   .connect(connectionString + dbname, { useNewUrlParser: true })
