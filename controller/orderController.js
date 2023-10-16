310f2f74bf589aa9b39ea2511144b360acf2b189
AldairAcero <zaracero68@gmail.com>
2023-10-16 11:46:07 -0600

    active for activo

--- controller/orderController.js
+++ controller/orderController.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
   const orderBody = req.body;
   let newOrder = new Order({
     ...orderBody,
-    status: "active",
+    status: "activo",
     date: new Date().toISOString().slice(0, 10).replace(/-/g, "/"),
     orderId: await getLastSequenceToday(),
   });
